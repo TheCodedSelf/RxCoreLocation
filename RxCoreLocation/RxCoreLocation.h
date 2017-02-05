f2b6acf49dcf8d251ffc1dad5ca1a6c0aab0c052
Keegan Rush <thecodedself@gmail.com>
2017-02-05 14:15:28 +0200

    Initial Commit

---
+++ RxCoreLocation/RxCoreLocation.h
@@ -0,0 +1,19 @@
+//
+//  RxCoreLocation.h
+//  RxCoreLocation
+//
+//  Created by Keegan Rush on 2017/02/05.
+//  Copyright © 2017 shnapped. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCoreLocation.
+FOUNDATION_EXPORT double RxCoreLocationVersionNumber;
+
+//! Project version string for RxCoreLocation.
+FOUNDATION_EXPORT const unsigned char RxCoreLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCoreLocation/PublicHeader.h>
+
+
